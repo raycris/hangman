41cbe2dd02daa4b3002886461e8fa4e3d72a7c29
Raycris Maldonado <raycris.maldonado@utreee.com>
2022-12-12 22:26:47 -0400

    creating the keyboard

--- src/components/keyboard/keyboard.tsx
+++ src/components/keyboard/keyboard.tsx
@@ -1,8 +1,40 @@
-import React from 'react'
+import styles from './keyboard.module.css'
+const KEYS = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+]
 
 const Keyboard = () => {
   return (
-    <div>Keyboard</div>
+    <div className={styles.container}>
+      {KEYS.map(item =>{
+        return <button key={item} className={`${styles["keyboard-btn"]} `}>{item}</button>
+      })}
+    </div>
   )
 }
 
